b137bef223004042a0d278a469aaee6d8dd9b8d0
Prajz <35326459+Prajz@users.noreply.github.com>
2023-01-28 18:55:51 +0000

    Initial Commit

---
+++ iSpendify/iSpendifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSpendifyApp.swift
+//  iSpendify
+//
+//  Created by Dev Prajapati on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iSpendifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
